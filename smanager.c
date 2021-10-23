08b6b18060a07523b225064d86922515b7bf5c84
ali019283 <aliburakcetin@protonmail.com>
2021-10-23 09:52:36 +0300

    update

--- smanager.c
+++ smanager.c
@@ -1,26 +1,19 @@
 #include<stdio.h>
-#include<signal.h>
+#include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
-enum halt{
-	REBOOT=1,
-	POWEROFF=2
-};
-int main(int argc, char const *argv[]){
-	for (int i = 0; i < argc; i++){
-		if (strcmp(argv[i], "reboot") == 0){
-			kill(1, SIGRTMIN+REBOOT);
-		}
-		else if (strcmp(argv[i], "poweroff") == 0){
-			kill(1, SIGRTMIN+POWEROFF);
-		}
-		else if (strcmp(argv[i], "enable") == 0){
-			char b[120];
-			sprintf(b, "/etc/seethe/services/%s", argv[i+1]);
-			char a[120];
-			sprintf(a, "/etc/seethe/runlevel/%s", argv[i+1]);
+#include<signal.h>
+int main(int argc, const char *argv[]){
+	if (argc < 3) exit(1);
+	if (!strcmp(argv[1], "enable")) {
+		for (int i = 2; i < argc; i++) {
+			char b[277], a[277];
+			snprintf(b, 277, "/etc/seethe/services/%s", argv[i]);
+			snprintf(a, 277, "/etc/seethe/runlevel/%s", argv[i]);
 			symlink(b, a);
 		}
 	}
+	else if (!strcmp(argv[1], "reboot")){kill(1, SIGRTMIN+1);}
+	else if (!strcmp(argv[1], "poweroff")){kill(1, SIGRTMIN+1);}
+	exit(0);
 }
-
