6ddbc25e61923947376ae7b51f9c896e92b38e51
ali019283 <aliburakcetin@protonmail.com>
2021-10-23 09:52:27 +0300

    update

--- seethe.c
+++ seethe.c
@@ -2,29 +2,18 @@
 #include<signal.h>
 #include<sys/reboot.h>  
 #include<unistd.h>
-enum halt{
-	REBOOT=1,
-	POWEROFF=2
-};
 void sig_handle(int signal, void (*handler)(int)){
     struct sigaction sa;
     sa.sa_handler = handler;
     sigaction(signal, &sa, NULL);
 }
-void poweroff(){
-	reboot(RB_POWER_OFF);	
-}
-void restart(){
-	reboot(RB_AUTOBOOT);
-}
+void poweroff(){reboot(RB_POWER_OFF);}
+void restart(){reboot(RB_AUTOBOOT);}
 int main(int argc, char *argv[]){
 	pid_t pid = fork();
-	if (pid == 0) {
-		execv("/etc/seethe/1", &argv[0]);
-	}
-	sig_handle(SIGRTMIN+REBOOT, restart);
-	sig_handle(SIGRTMIN+POWEROFF, poweroff);
-	while(1)
-		pause();
+	if (pid == 0){execv("/etc/seethe/1", &argv[0]);}
+	sig_handle(SIGRTMIN+1, restart);
+	sig_handle(SIGRTMIN+1, poweroff);
+	while(1) {pause();}
 	return 0;
 }
