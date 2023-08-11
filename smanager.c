7f7f88b8aaa3ea80e877c8f1157ad5eebbb73fbe
Ali Burak Çetin <aliburakcetin@protonmail.com>
2023-08-12 01:45:15 +0300

    added features and verbose output

--- smanager.c
+++ smanager.c
@@ -4,21 +4,27 @@
 #include<unistd.h>
 #include<signal.h>
 int main(int argc, const char *argv[]){
-	if (argc < 2){puts("It needs some flags to work properly, unfortunately this program can't understand what you want by mind reading."); exit(1);}
+	if (argc < 2){puts("err: too few arguments"); exit(1);}
+	if (geteuid() != 0){puts("err: root privileges needed"); exit(1);}	
 	for (int i = 0; i < argc; i++){
 		if (!strcmp(argv[i], "enable")){
 			for (int b = i+1; b < argc; b++){
-				if (!strcmp(argv[b], "reboot") || !strcmp(argv[b], "poweroff" || !strcmp(argv[b], "disable"))){i = b; break;}
+				if (!strcmp(argv[b], "reboot") || !strcmp(argv[b], "poweroff") || !strcmp(argv[b], "disable")){i = b; break;}
 				char a[277], c[277];
 				snprintf(a, 277, "/etc/seethe/services/%s", argv[b]);
 				snprintf(c, 277, "/etc/seethe/runlevel/%s", argv[b]);
+				if (!access(a, F_OK)) {printf("enabling service %s...\n", argv[b]);}
+				else {printf("'%s' could not be found\n", argv[b]); break;}
+				if (!access(c, F_OK)) {printf("service '%s' already enabled, enabling again...\n", argv[b]);}
 				symlink(a, c);
 			}
 		}
 		if (!strcmp(argv[i], "disable")){
 			for (int n = i+1; n < argc; n++){
 				char c[277];
-				snprintf(c, 277, "/etc/seethe/runlevel/%s", argv[n]); 
+				snprintf(c, 277, "/etc/seethe/runlevel/%s", argv[n]);
+				if (access(c, F_OK)) {printf("service '%s' already disabled or removed, disabling again...\n", argv[n]);}
+				else{printf("disabling service %s...\n", argv[n]);}
 				unlink(c); break;
 			}
 		}
